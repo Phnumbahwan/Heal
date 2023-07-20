2fba2e8e9fa9d77900d14557d2197d596634a35e
Free Wifi <quickerz59@gmail.com>
2023-07-20 13:59:20 +0800

    Fixed image

--- src/components/GetAldrin.jsx
+++ src/components/GetAldrin.jsx
@@ -1,7 +1,7 @@
 const GetAldrin = () => {
     return (
-        <div>
-            <img src={'/src/assets/aldrin.png'} alt="Image" />
+        <div className="w-[500px] h-[500px] rounded-full overflow-hidden">
+            <img className="w-full h-full object-cover" src={'https://raw.githubusercontent.com/Phnumbahwan/Heal/main/src/assets/aldrin.png'} alt="Image" />
         </div>
     )
 }
